d5703ee1914f6960ad066c87d63c4a467d750674
StathisKap <stathiskap75@gmail.com>
2020-12-01 20:22:16 +0000

    Made the Mode work using two for loops

--- Statistic.c
+++ Statistic.c
@@ -1,8 +1,8 @@
-/* Finds the Mean, Standatd deviation, variance and Coefficient of Variation of a file
+/* Finds the Mean, Standatd deviation, variance, median and Coefficient of Variation of a file
 *Author: Stathis Kapnidis
 *Date Created: 1/12/2020
 *Last Modified:1/12/2020
-*TODO: Add mode and median
+*TODO: Add mode
 */
 
 #include <stdio.h>
@@ -14,7 +14,7 @@ void main(int argc, char *argv[])
     char *name = argv[1];
     FILE * input = fopen(name,"r");
     int count = 0;
-    float mean=0,variance=0,standard_deviation=0, measurements=0,median =0;
+    float mean=0,variance=0,standard_deviation=0, measurements=0,median =0, mode=0,measurements2 =0;
 
         //Scan the file for all the values and find the mean
     while(EOF && fscanf(input, "%f", &measurements)==1)
@@ -38,7 +38,7 @@ void main(int argc, char *argv[])
         //Reset Position
     fseek(input, 0, SEEK_SET);
 
-        //Finding the median
+        //Find the median
     if(count%2==0)
     {       //If the lines are even, then add the 2 middle values and find the average
         for(int i = 0; i<((count/2)-1);i++)
@@ -60,9 +60,28 @@ void main(int argc, char *argv[])
         }
         median += measurements; 
     }
-       
+        //Reset Position
+    fseek(input, 0, SEEK_SET);
 
-    printf("Mean = %.3f\nVariance = %.3f\nStandard Deviation = %.3f\nCoefficient of Variation = %.3f\nMedian = %.3f\n",mean,variance,standard_deviation,standard_deviation/mean,median);
+        //Finding the mode
+        int i = 0, j = 0, maxOccurances = 0;
+    for (i = 0; i < count; ++i) {
+        int occurances= 0;
+        fscanf(input,"%f",&measurements);
+      
+      for (j = 0; j < count; ++j) {  
+        fscanf(input,"%f",&measurements2);
+        if (measurements2 == measurements)
+        ++occurances;
+      }
+      
+      if (occurances > maxOccurances) {
+         maxOccurances = occurances;
+         mode = measurements;
+      }
+   }
+
+    printf("Mean = %.3f\nVariance = %.3f\nStandard Deviation = %.3f\nCoefficient of Variation = %.3f\nMedian = %.3f\nMode = %.3f\n",mean,variance,standard_deviation,standard_deviation/mean,median,mode);
     fclose(input);
 
 }
